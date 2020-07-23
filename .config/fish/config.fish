6988ab46b64cc1cf3bfee3484c7774ef45c5afe9
Kelly Runnels <kcrunnels15@gmail.com>
2020-07-23 12:42:43 -0400

    Added binding for something herbstluftwm.

--- .config/fish/config.fish
+++
@@ -1,8 +0,0 @@
-alias pkginstall="sudo pacman -S"
-alias pkgsearch="pacman -Ss"
-alias pkginfo="pacman -Sii"
-alias pkgrm="sudo pacman -R"
-alias pkgupdate="sudo pacman -Syu"
-alias get="git clone"
-alias vim="nvim"
-alias config='/usr/bin/git --git-dir=/home/kellyr/dotfiles/ --work-tree=/home/kellyr'
